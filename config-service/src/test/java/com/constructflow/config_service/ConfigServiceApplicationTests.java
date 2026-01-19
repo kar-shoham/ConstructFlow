93fb420b9b64c47323493ef3618457d4defeefa5
kar-shoham <kar.shoham.25@gmail.com>
2026-01-20 00:12:15 +0530

    Initial commit

---
+++ config-service/src/test/java/com/constructflow/config_service/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructflow.config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ entity-service/src/test/java/com/constructflow/entity_service/EntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructflow.entity_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
