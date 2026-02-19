50129225ed1a0fdffaf69c4ab98d27569627a1a6
kar-shoham <kar.shoham.25@gmail.com>
2026-02-19 07:45:01 +0530

    Add: payroll-service files and docker compose-payroll-service-db, kafka, kafbat, scheam registry

---
+++ payroll-service/src/test/java/com/constructflow/payroll_service/PayrollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructflow.payroll_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
